20a8f059fca7282c77881ff672078be1edbc6884
Michael Wheeler <michaelwheeler@michaelwheeler.io>
2024-08-04 20:41:11 -0400

    Empty modules for two new response handlers that we need

--- application/response_handlers/album_tracks.py
+++ application/response_handlers/album_tracks.py
@@ -1,3 +1,3 @@
 # FIXME: write a response handler to map to this
 
-URL_PATTERN = "https://api.spotify.com/v1/albums/*/tracks"
+URL_PATTERN = "https://api.spotify.com/v1/albums/{id}/tracks"
---
+++ application/response_handlers/artist_albums.py
@@ -0,0 +1,3 @@
+# FIXME: write a response handler for this
+
+URL_PATTERN = "https://api.spotify.com/v1/albums/{id}/tracks"
